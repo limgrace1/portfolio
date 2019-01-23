2154bafe70f41135ed6be513a9d308c1b701e337
Grace Lim <limgrace96@gmail.com>
2019-01-22 16:09:22 -0800

    update 9

--- portfolio.js
+++ portfolio.js
@@ -2,30 +2,35 @@
 
 // Scroll Reveal //
 
-ScrollReveal().reveal('.display-4');
-
-
 (function scrollReveal() {
-    window.sr = ScrollReveal();
-    
-    sr.reveal('.card-img-top', {
-      duration   : 600,
-      distance   : '20px',
-      easing     : 'ease-out',
-      origin     : 'bottom',
-      reset      : true,
-      scale      : 1,
-      viewFactor : 0,
-      afterReveal  : revealChildren,
-    }, 150);
-    
-      var revealChildren = sr.reveal('.card-title, .card-text', {
-      duration   : 500,
-      scale      : 1,
-      distance   : '20px',
-      origin     : 'bottom',
-      reset      : true,
-      easing     : 'ease-out',
-      viewFactor : 1,
-    }, 75);
-  })();
+  window.sr = ScrollReveal();
+  
+  sr.reveal('.card', {
+    duration   : 600,
+    distance   : '20px',
+    easing     : 'ease-out',
+    origin     : 'bottom',
+    reset      : true,
+    scale      : 1,
+    viewFactor : 0,
+    afterReveal  : revealChildren,
+  }, 150);
+  
+    var revealChildren = sr.reveal('.card-title, .card-text', {
+    duration   : 500,
+    scale      : 1,
+    distance   : '20px',
+    origin     : 'bottom',
+    reset      : true,
+    easing     : 'ease-out',
+    viewFactor : 1,
+  }, 75);
+})();
+
+
+// CAROUSEL //
+
+$('.carousel-inner').carousel({
+  interval: 200
+})
+
